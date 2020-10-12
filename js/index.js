6639c28bfbc571d1e51ef2a4e5d7cd326b41994e
Elisenda <elisenda.monserrat@gmail.com>
2020-10-12 18:07:11 +0200

    Finished exercise and did bonus 1

--- js/index.js
+++ js/index.js
@@ -1,46 +1,118 @@
-// Iteration 1: Names and Input
-let hacker1 = "Elisenda"
-console.log(`The drivers name is ${hacker1}`)
+//ITERATION 1
+  //1.1 Create a variable `hacker1` with the driver's name.
+  const hacker1 = "Laura";
 
-let hacker2 = "Felipe"
-console.log(`The navigator name is ${hacker2}`)
+  //1.2 Print `"The driver's name is XXXX"`.
+  console.log("Driver's name -->", hacker1);
 
-// Iteration 2: Conditionals > Depending on which name is longer, print:
-let driver = hacker1.length
-let navigator = hacker2.length
+  //1.3 Create a variable `hacker2` with the navigator's name.
+  const hacker2 = "Pablo";
 
-if (driver > navigator) {
-  console.log(`The driver has the longest name, it has ${driver} characters`)
-} else if (driver < navigator) {
-  console.log(`It seems that the navigator has the longest name, it has ${navigator} characters`)
-} else if (driver == navigator) {
-  console.log("Wow, you both have equally long names, characters!")
+  //1.4 Print `"The navigator's name is YYYY"`.
+  console.log("Navigator's name -->", hacker2);
+
+//ITERATION 2: Conditionals
+//Depending on which name is longer print different outputs
+
+let hacker1Length = hacker1.length;
+let hacker2Length= hacker2.length;
+
+console.log("Driver's name length -->", hacker1Length);
+console.log("Navigator's name length -->",  hacker2Length);
+ 
+if (hacker1Length > hacker2Length) {
+    console.log(`The driver has the longest name, it has ${hacker1Length} characters`);
+} else if (hacker1Length < hacker2Length) {
+   console.log(`It seems that the navigator has the longest name, it has ${hacker2Length} characters.`);
 } else {
-  console.log("Woow, you're both a hackers")
+    console.log(`Wow, you both have equally long names, ${hacker1Length} characters!`);
 }
 
-// Iteration 3: Loops
+//ITERATION 3: Loops 
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N" 
 
-// 3.1 Print all the characters of the navigators driver's name,separated by a space and in capital
+    let driverNameUpperCase = hacker1.toUpperCase().split("");
+    console.log(driverNameUpperCase);
 
-let hacker2Capital = hacker2.toUpperCase();
+    // Using for...of Loop
+    for ( let nameUpperCase of driverNameUpperCase) {
+        console.log(nameUpperCase.split(""));
+    }
 
-console.log(hacker2Capital.split(""));
+    // Using for loop
+    for (i = 0; i < driverNameUpperCase.length; i++ ) {
+      console.log(driverNameUpperCase[i]);
+    } 
 
-/* Comment 3.1: He podido hacer el ejercicio pero sin utilizar un loop. Cuando intento usar un loop, 
-no consigo que los caracteres estén separados ni en la misma fila */
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
 
-for (let value of hacker2Capital) {
- console.log(value);
-}
+    console.log(hacker1.length -1);
 
-// 3.2 Print all the characters of the navigator's name, in reverse order
+    // Reverse a String With a Decrementing For Loop
+    for (i= hacker1Length -1; i >= 0; i--){
+      console.log(hacker1[i]);
+    }
 
-let reverseNavigator = "";
+    // With a function - Reverse a String With a Decrementing For Loop
 
-for ( let i = hacker2.length - 1; i >= 0 ; i--) {
-  reverseNavigator += hacker2[i];
-}
+    let newStr = "";
 
-console.log(reverseNavigator);
+    function reverseString(str) {
+      for (i = str.length -1; i >= 0; i--){
+        newStr += str[i];
+      }
+      return newStr;
+    }
+
+    reverseString("Eli");
+    console.log(newStr);
+
+    //Reverse a String With Built-In Functions
+  
+    function reverseString2(str) {
+      let splitString = str.split("");
+      //console.log(splitString);
+      let reverseStr = splitString.reverse();
+      //console.log(reverseStr);
+      let joinStr = reverseStr.join("");
+      //console.log(joinStr);
+      return joinStr
+    }
+    
+    console.log(reverseString2("Elisenda"));
+
+
+//3.3 Depending on the lexicographic order of the strings, print:
+
+  // strings being: hacker1 = "Laura" & hacker2 = "Pablo"
+
+    if (hacker1.localeCompare(hacker2) === -1) {
+      console.log("The driver's name goes first.");
+    } else if (hacker1.localeCompare(hacker2) === 1){
+      console.log("Yo, the navigator goes first definitely.");
+    } else {
+      console.log("What?! You both have the same name?");
+    }
+
+/*BONUS 1
+    Go to lorem ipsum generator and:
+
+      1. Generate 3 paragraphs. Store the text in a variable type of string.
+      2. Make your program count the number of words in the string.
+      3. Make your program count the number of times the Latin word et appears.
+    
+*/
+    const loremIpsumText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet ligula et sem sodales convallis. Mauris elit dui, dictum et est vel, placerat mollis risus. Phasellus ac convallis lectus. Sed scelerisque nisi sit amet lobortis maximus. Suspendisse potenti. Aenean vitae mauris in nisl posuere sagittis quis sed ante. Curabitur vel rhoncus mi. Etiam quis lacus ullamcorper, tristique ligula vel, laoreet nibh. Duis vehicula ornare dolor, a euismod est dictum aliquet."
+
+    function countStringWords(str) {
+      let words = 0;
+      let splitString = str.split(" ");
+      let stringWords = splitString.length;
+      return words += stringWords;
+    }
+
+      countStringWords(loremIpsumText);
+
+      let countEtTimes = loremIpsumText.search("et");
+      console.log(countEtTimes);
 
